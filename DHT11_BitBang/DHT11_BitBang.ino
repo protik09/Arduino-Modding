8f993558c82fb9176632e77eb5432a0e5e56e69a
Protik Banerji <protik09@gmail.com>
2021-03-09 01:10:29 +0100

    Get the ack working for DHT11 library

--- DHT11_BitBang/DHT11_BitBang.ino
+++ DHT11_BitBang/DHT11_BitBang.ino
@@ -1,48 +1,72 @@
 /**
  * @file DHT11_BitBang.ino
- * 
+ *
  * @author Protik Banerji (protik09@gmail.com)
  * @brief Bit Bang the DHT11 Protocol with a Teensy 4.0
  * @version 0.1
  * @date 2021-03-04
- * 
+ *
  * @copyright Copyright (c) 2021
- * 
+ *
  */
 
 #include <Arduino.h>
 #include "dht11.h"
 
-volatile bool linelevel;
-elapsedMillis timer_mil;
-elapsedMicros timer_mic;
-unsigned long response;
+// Globals
+bool exitFlag = false;
+Dht11 dht11(DHT11PIN); // Init DHT11 object
 
+// SETUP
 void setup()
 {
+    // Calculate DHT11 datarate
+    uint32_t dht11_micros_per_bit = uint32_t(float(1 / DHT11_BITRATE) * 1000000);
+
+    // Starup serial port
     Serial.begin(115200);
     while (!Serial); // wait for Arduino Serial Monitor
+
     delay(500);
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWriteFast(LED_BUILTIN, HIGH); //
+    digitalWriteFast(LED_BUILTIN, HIGH);
     Serial.println("Micontroller setup complete");
+    Serial.println("The sensor data rate is set to " + String(dht11_micros_per_bit) + "us per bit");
+    delay(2000);
+
+    // Probably only useful with teensy till I test otherwise
+    // while(!startup())
+    // {
+    //     // Do nothing till user action is received.
+    // };
+    // digitalWriteFast(LED_BUILTIN, HIGH);
 }
 
+// Infinite loop
 void loop()
 {
     // Request DHT11
-    Serial.println("Sending initial ping to DHT11");
-    pinMode(DHT11PIN, OUTPUT);
-    digitalWriteFast(DHT11PIN, HIGH); //Initial pin high before pulling low
-    digitalWriteFast(DHT11PIN, LOW);
-    delay(18); // Wait 18ms
-    digitalWriteFast(DHT11PIN, HIGH); //Initial pin high before pulling low
-    delayMicroseconds(40); // Wait 40us
+    dht11.Request();
+
     // Listen to DHT11
-    pinMode(DHT11PIN, INPUT);
-    timer_mic = 0;
-    while (digitalReadFast(DHT11PIN) == HIGH)
+    if (dht11.Ack() == true)
     {
-        ;
-    } 
+        Serial.println("SUCCESS: Sensor Replied to your dulcet tones.");
+    }
+    else
+    {
+        exit();// Do nothing since error handling is done in the class.
+    }
+}
+
+// Exit loop
+void exit(int exit_code)
+{
+    digitalWriteFast(LED_BUILTIN, LOW);
+    Serial.println("Program Ended. Exit code: " + String(exit_code));
+    while (true)
+    {
+        ; // Do nothing program halt
+    }
+    
 }
--- DHT11_BitBang/dht11.h
+++ DHT11_BitBang/dht11.h
@@ -11,7 +11,118 @@
  * 
  */
 
+// Since the smallest bit received from the DHT11 takes 24us, we can assume a communication rate of 41666 bps.
+
 #define DHT11PIN 8u
+#define DHT11_BITRATE 41666 // 41666.67 bps
+// #define DHT11_MICROS_PER_BIT (float(1 / DHT11_BITRATE) * 1000000)
+#define DHT11_ACK_RESPONSE1 55u
+#define DHT11_ACK_RESPONSE2 80u
+#define ACK HIGH
+// #define Serial Serial1 // Remove before compile. Hack for vscode linting
+// #define NO_ACK LOW
 
 extern volatile bool linelevel;
+
+// Function definitions
+bool startup();
+void exit(int exit_code = 0);
+
+// Class Definitions
+class Dht11
+{
+    private:
+        uint8_t sensor_pin;
+    public:
+        Dht11::Dht11(uint8_t sensor_pin);
+        void Request();
+        bool Ack();
+};
+
+Dht11::Dht11(uint8_t _sensor_pin)
+{
+    sensor_pin = _sensor_pin;
+    pinMode(sensor_pin, OUTPUT);
+    digitalWriteFast(sensor_pin, HIGH);  //Initial pin high to sensor
+    delay(1000); // Wait 1s after powerup to get good readings
+}
+
+void Dht11::Request()
+{
+    pinMode(sensor_pin, OUTPUT);
+    digitalWriteFast(sensor_pin, LOW);
+    delay(20);                           // Wait > 18ms 
+    digitalWriteFast(sensor_pin, HIGH);  //Initial pin high before pulling low
+    delayMicroseconds(13);               // Wait at least 10-20us
+}
+
+bool Dht11::Ack()
+{
+    uint16_t low_counter = 0, high_counter = 0;
+    uint16_t timeout = 100;
+    elapsedMicros time_count = 0;
+    pinMode(sensor_pin, INPUT);
+
+    uint16_t old_time = 0;
+    // Read ACK low
+    while (digitalReadFast(sensor_pin) == LOW)
+    {
+        if ((low_counter < timeout) && (time_count > old_time))
+        {
+            low_counter++;
+        }
+        else if (low_counter == timeout)
+        {
+            Serial.println("ERROR: Timed out on receive LOW. Low Counter: " + String(low_counter));
+            return !ACK;
+        }
+        else
+        {
+            ;// Do nothing
+        }
+        
+        old_time = time_count;
+    }
+
+    // Read ACK high
+    old_time = 0;
+    if (low_counter <= 81)
+    {
+        while (digitalReadFast(sensor_pin) == HIGH)
+        {
+            if ((high_counter < timeout) && (time_count > old_time))
+            {
+                high_counter++;
+            }
+            else if(high_counter == timeout)
+            {
+                Serial.println("ERROR: Timed out on receive HIGH. High Counter: " + String(high_counter));
+                return !ACK;
+            }
+            else
+            {
+                ; // Do Nothing
+            }
+        }
+        old_time = time_count;
+    }
+    else
+    {
+            Serial.println("ERROR: DHT11 Took too long to reply. Low Counter: " + String(low_counter));
+            return !ACK;
+    }
+    
+
+    // Check low and high count
+    if ((low_counter <= 81) && (high_counter <= 81))
+    {
+        return ACK;
+    }
+    else
+    {
+        Serial.println("ERROR: DHT11 Took too long to reply. Low Counter: " + String(low_counter));
+        Serial.println("ERROR: DHT11 Took too long to reply. High Counter: " + String(high_counter));
+        return !ACK;
+    }
+}
 
